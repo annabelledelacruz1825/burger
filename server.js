4d5ab2aacef3301b8d71523d1d05fd7e33eb37c8
annabelle <annabelld@baptisthealth.net>
2019-11-04 16:51:35 -0500

    added deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
 
@@ -21,9 +21,19 @@ app.set('view engine', 'handlebars');
 var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
 
+mongoose.promise = global.Promise;
 
 
-app.listen(PORT, function() 
+// Connect to the Mongo DB
+mongoose.connect
+(
+  process.env.MONGODB_URI || "mongodb://user1:password1@ds241288.mlab.com:41288/heroku_smvghxlh",
+  {
+    useMongoClient: true
+  }
+  );
+
+
 {
   console.log("App listening on PORT " + PORT);
 });
